023716416a9a18fb837c84ec7681cd407edb8b36
Remy <remyrussell1@gmail.com>
2023-09-10 15:54:03 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,62 +1,68 @@
-document.addEventListener("DOMContentLoaded", function() {
+// Function to inject content from the JSON into the HTML
+function populateResume(data) {
+    document.getElementById("name").textContent = data.name;
+    document.getElementById("phone").textContent = data.phone;
+    document.getElementById("email").textContent = data.email;
+
+    const productManagementList = document.getElementById("productManagement");
+    data.productManagement.forEach(item => {
+        let listItem = document.createElement("li");
+        listItem.textContent = item;
+        productManagementList.appendChild(listItem);
+    });
+
+    const professionalExperienceDiv = document.getElementById("professionalExperience");
+    data.professionalExperience.forEach(exp => {
+        let jobDiv = document.createElement("div");
+        let title = document.createElement("h3");
+        title.textContent = `${exp.role} – ${exp.team}, ${exp.company}`;
+        let duration = document.createElement("p");
+        duration.textContent = `${exp.startDate} – ${exp.endDate}`;
+        jobDiv.appendChild(title);
+        jobDiv.appendChild(duration);
+        if (exp.details) {
+            let details = document.createElement("p");
+            details.textContent = exp.details;
+            jobDiv.appendChild(details);
+        }
+        exp.bullets.forEach(bullet => {
+            let bulletItem = document.createElement("p");
+            bulletItem.textContent = `• ${bullet}`;
+            jobDiv.appendChild(bulletItem);
+        });
+        professionalExperienceDiv.appendChild(jobDiv);
+    });
+
+    const educationDiv = document.getElementById("education");
+    let degree = document.createElement("p");
+    degree.textContent = `${data.education.degree}, ${data.education.specialization} ${data.education.year} - ${data.education.institution}`;
+    educationDiv.appendChild(degree);
+
+    const skillsDiv = document.getElementById("skills");
+    skillsDiv.innerHTML = `
+        <strong>Skills:</strong> ${data.skills.join(", ")}<br>
+        <strong>Tools/Frameworks:</strong> ${data.tools.join(", ")}<br>
+        <strong>Fun:</strong> ${data.fun.join(", ")}
+    `;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
     fetch('resume.json')
         .then(response => response.json())
         .then(data => {
-            document.getElementById("name").textContent = data.name;
-            document.getElementById("contact").textContent = `${data.phone} | ${data.email}`;
-            document.getElementById("role").textContent = data.role;
+            populateResume(data);
 
-            const productManagementList = document.getElementById("productManagementList");
-            data.productManagement.forEach(item => {
-                let li = document.createElement("li");
-                li.textContent = item;
-                productManagementList.appendChild(li);
+            // Toggle Dark Mode functionality
+            const toggleButton = document.getElementById("toggleMode");
+            toggleButton.addEventListener("click", function() {
+                const currentMode = document.body.className;
+                if (currentMode === "light-mode") {
+                    document.body.className = "dark-mode";
+                    toggleButton.textContent = "Switch to Light Mode";
+                } else {
+                    document.body.className = "light-mode";
+                    toggleButton.textContent = "Switch to Dark Mode";
+                }
             });
-
-            const professionalExperienceList = document.getElementById("professionalExperienceList");
-            data.professionalExperience.forEach(exp => {
-                let expDiv = document.createElement("div");
-                expDiv.innerHTML = `
-                    <h3>${exp.position}, ${exp.company}</h3>
-                    <p>${exp.location}</p>
-                    <p>${exp.duration}</p>
-                    <p>${exp.description}</p>
-                    <ul>
-                        ${exp.highlights.map(highlight => `<li>${highlight}</li>`).join('')}
-                    </ul>
-                `;
-                professionalExperienceList.appendChild(expDiv);
-            });
-
-            document.getElementById("education").innerHTML = `
-                <h3>${data.education.degree}</h3>
-                <p>${data.education.institution}</p>
-                <p>${data.education.coursework}</p>
-            `;
-
-            const skillsList = document.getElementById("skillsList");
-            data.skills.skills.forEach(skill => {
-                let li = document.createElement("li");
-                li.textContent = skill;
-                skillsList.appendChild(li);
-            });
-
-            const toolsList = document.getElementById("toolsList");
-            data.skills.toolsAndFrameworks.forEach(tool => {
-                let li = document.createElement("li");
-                li.textContent = tool;
-                toolsList.appendChild(li);
-            });
-
-            const funList = document.getElementById("funList");
-            data.skills.fun.forEach(fun => {
-                let li = document.createElement("li");
-                li.textContent = fun;
-                funList.appendChild(li);
-            });
-
-        })
-        .catch(error => {
-            console.error("There was an error fetching or parsing the JSON:", error);
         });
 });
