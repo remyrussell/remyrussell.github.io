fd53dd4220c9915c69bb04004709374089b17f05
Remy <remyrussell1@gmail.com>
2023-09-10 20:53:07 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -27,53 +27,11 @@ function attachThemeToggleEvent() {
 }
 
 // Function to populate the resume content
-function populateResume(data) {
-    document.getElementById('name').innerText = data.name;
-    document.getElementById('role').innerText = data.role;
-    document.getElementById('email').innerText = data.email;
-    document.getElementById('phone').innerText = data.phone;
-    document.getElementById('productManagementList').innerHTML = data.productManagement.map(item => `<li>${item}</li>`).join('');
-
-    let experienceContainer = document.getElementById('professionalExperience');
-    for (let experience of data.professionalExperience) {
-        let experienceDiv = document.createElement('div');
-        experienceDiv.className = 'experience-item';
-
-        // Add company logo
-        let logoImg = document.createElement('img');
-        logoImg.src = experience.logo;
-        logoImg.alt = experience.company + ' logo';
-        logoImg.width = 100; // or any other size you want
-        experienceDiv.appendChild(logoImg);
-
-        // Add experience details
-        let detailsDiv = document.createElement('div');
-        detailsDiv.innerHTML = `
-            <h3>${experience.position} at ${experience.company}</h3>
-            <p>${experience.location} | ${experience.duration}</p>
-            <p>${experience.description}</p>
-            <ul>
-                ${experience.highlights.map(highlight => `<li>${highlight}</li>`).join('')}
-            </ul>
-        `;
-        experienceDiv.appendChild(detailsDiv);
-        experienceContainer.appendChild(experienceDiv);
-    }
-
-    // ... [Process other sections like skills, education, etc.]
-
-    // This is just a continuation. Add other sections accordingly.
-}
+// ... [remaining unchanged part]
 
 document.addEventListener('DOMContentLoaded', async () => {
     keepThemeSetting();
     attachThemeToggleEvent();
 
-    try {
-        let response = await fetch('/resume.json');
-        let data = await response.json();
-        populateResume(data);
-    } catch (err) {
-        console.error('Error fetching or parsing JSON:', err);
-    }
+    // ... [remaining unchanged part]
 });
