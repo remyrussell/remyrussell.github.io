441f4337fbd7734db1c2b74e9e3fd7761085e937
Remy <remyrussell1@gmail.com>
2023-09-10 21:02:03 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 // Functions related to theme toggling
 function setTheme(themeName) {
+    console.log('Setting theme to:', themeName); // Added for debugging
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
 }
 
 function toggleTheme() {
+    console.log('Toggling theme...'); // Added for debugging
     if (localStorage.getItem('theme') === 'theme-dark') {
         setTheme('theme-light');
     } else {
@@ -23,15 +25,61 @@ function keepThemeSetting() {
 // Event attachment for theme toggling
 function attachThemeToggleEvent() {
     let themeToggleButton = document.getElementById('themeToggleButton');
-    themeToggleButton.addEventListener('click', toggleTheme);
+    if(themeToggleButton) { // Check if the button exists
+        themeToggleButton.addEventListener('click', toggleTheme);
+    } else {
+        console.error('Theme toggle button not found!'); // Log an error if the button isn't found
+    }
 }
 
 // Function to populate the resume content
-// ... [remaining unchanged part]
+function populateResume(data) {
+    document.getElementById('name').innerText = data.name;
+    document.getElementById('role').innerText = data.role;
+    document.getElementById('email').innerText = data.email;
+    document.getElementById('phone').innerText = data.phone;
+    document.getElementById('productManagementList').innerHTML = data.productManagement.map(item => `<li>${item}</li>`).join('');
+
+    let experienceContainer = document.getElementById('professionalExperience');
+    for (let experience of data.professionalExperience) {
+        let experienceDiv = document.createElement('div');
+        experienceDiv.className = 'experience-item';
+
+        // Add company logo
+        let logoImg = document.createElement('img');
+        logoImg.src = experience.logo;
+        logoImg.alt = experience.company + ' logo';
+        logoImg.width = 100; // or any other size you want
+        experienceDiv.appendChild(logoImg);
+
+        // Add experience details
+        let detailsDiv = document.createElement('div');
+        detailsDiv.innerHTML = `
+            <h3>${experience.position} at ${experience.company}</h3>
+            <p>${experience.location} | ${experience.duration}</p>
+            <p>${experience.description}</p>
+            <ul>
+                ${experience.highlights.map(highlight => `<li>${highlight}</li>`).join('')}
+            </ul>
+        `;
+        experienceDiv.appendChild(detailsDiv);
+        experienceContainer.appendChild(experienceDiv);
+    }
+
+    // ... [Process other sections like skills, education, etc.]
+
+    // This is just a continuation. Add other sections accordingly.
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
     keepThemeSetting();
     attachThemeToggleEvent();
 
-    // ... [remaining unchanged part]
+    try {
+        let response = await fetch('/resume.json');
+        let data = await response.json();
+        populateResume(data);
+    } catch (err) {
+        console.error('Error fetching or parsing JSON:', err);
+    }
 });
